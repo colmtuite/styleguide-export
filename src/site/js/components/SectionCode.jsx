c0380b0db18cec9940dabc557e44991395bbff08
Yavor Punchev <yavor.punchev@gmail.com>
2016-04-05 18:52:57 +0100

    Highlight js tahnks to Paolo

--- src/site/js/components/SectionCode.jsx
+++ src/site/js/components/SectionCode.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import hljs from 'highlight.js';
 
-export default function SectionCode({ code }) {
-    return (
-        <div className="padding-m bg-snow borderWidth-0 borderRightWidth-1 borderBottomWidth-1 borderLeftWidth-1 borderStyle-solid borderColor-smoke borderRadiusBottom-m">
-            <pre>
-                <code>{ code }</code>
-            </pre>
-        </div>
-    );
+export default class SectionCode extends React.Component {
+    componentDidMount() {
+        this.highlight();
+    }
+
+    componentDidUpdate() {
+        this.highlight();
+    }
+
+    highlight() {
+        if (this.refs.code) {
+            hljs.highlightBlock(this.refs.code);
+        }
+    }
+
+    render() {
+        const { code } = this.props;
+        return (
+            <div className="padding-m bg-snow borderWidth-0 borderRightWidth-1 borderBottomWidth-1 borderLeftWidth-1 borderStyle-solid borderColor-smoke borderRadiusBottom-m">
+                <pre>
+                    <code ref="code">{ code }</code>
+                </pre>
+            </div>
+        );
+    }
 }
 
 SectionCode.propTypes = {
