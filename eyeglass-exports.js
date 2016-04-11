fc4e923e32b2198773ca207ec4119c984c162c75
George Reith <george@marvelapp.com>
2016-04-11 13:59:16 +0100

    Fix eyeglass exports

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -1,14 +1,8 @@
 const path = require('path');
 
-module.exports = () => ({
-    assets: {
-        httpPrefix: 'assets',
-        relativeTo: '/marvel-styleguide',
-        sources: [{
-            directory: path.resolve(__dirname, 'src', 'assets'),
-            globOpts: {
-                pattern: 'images/**/*',
-            },
-        }],
-    },
-});
+module.exports = eyeglass => {
+    eyeglass.assets.addSource(path.resolve(__dirname, 'src', 'assets'), {
+        pattern: 'images/**/*',
+    });
+    return eyeglass;
+};
