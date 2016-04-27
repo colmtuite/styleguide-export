a9eca516bd2d7e427165bed6177a97f72edb314f
Colm Tuite <colmtuite@gmail.com>
2016-04-28 00:30:18 +0100

    No.

--- src/site/js/App.react.js
+++ src/site/js/App.react.js
@@ -13,8 +13,6 @@ export default function App(props) {
             <Sidebar />
 
             <div className="main-content">
-                <div style={{ height: '108px' }}></div>
-
                 <div className="posFixTL zi-chrome bg-white width-100 textAlign-center boxShadow-close breakPointM-display-none">
                     <SidebarLogo />
                 </div>
