8b747455ef0143de11992e06b1ddab25b094e24c
George Reith <george@marvelapp.com>
2016-05-03 16:50:40 +0100

    Import history. Check web pack config for outputPath and make history use it as base name. Split App into App and Page and wrap everything in base route

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = function(eyeglass){
+module.exports = function exportEyeglass(eyeglass) {
     eyeglass.assets.addSource(path.resolve(__dirname, 'src', 'assets'), {
         pattern: 'images/**/*',
     });
--- src/site/js/pages/404.react.js
+++ src/site/js/pages/404.react.js
@@ -1,27 +1,21 @@
 import React from 'react';
 
-import Sidebar from '~/Sidebar.react';
+import Page from '~/pages/Page.react';
 
 export default function NoMatch() {
     return (
-        <div>
-            <Sidebar />
-
-            <div style={{ paddingLeft: '20%' }}>
-                <div
-                    className="padding-xl marginLeftRight-m"
-                    style={{ maxWidth: '960px' }}>
-                    <div className="marginTop-l marginBottom-m">
-                        <h1 className="fontSize-xxxl">
-                            Page not found
-                        </h1>
-                    </div>
-
-                    <p className="lineHeight-l fontSize-l fontWeight-3 c-silver">
-                        The page you’re looking for could have been deleted or never have existed.
-                    </p>
+        <Page>
+            <div>
+                <div className="marginTop-l marginBottom-m">
+                    <h1 className="fontSize-xxxl">
+                        Page not found
+                    </h1>
                 </div>
+
+                <p className="lineHeight-l fontSize-l fontWeight-3 c-silver">
+                    The page you’re looking for could have been deleted or never have existed.
+                </p>
             </div>
-        </div>
+        </Page>
     );
 }
