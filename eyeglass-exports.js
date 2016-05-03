18fa25e1a541566d0cb6783586c4d310a265cbbf
Brendan Moore <brendannmoore@gmail.com>
2016-05-03 13:08:59 +0100

    Update eyeglass-exports.js

--- eyeglass-exports.js
+++ eyeglass-exports.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = eyeglass => {
+module.exports = eyeglass = function(){
     eyeglass.assets.addSource(path.resolve(__dirname, 'src', 'assets'), {
         pattern: 'images/**/*',
     });
