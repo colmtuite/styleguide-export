f5fc05c2a1acd777f75dc45d290dd548c5886cbe
Yavor Punchev <yavor.punchev@gmail.com>
2016-05-04 12:40:41 +0100

    Small bugs

--- src/site/js/App.react.js
+++ src/site/js/App.react.js
@@ -9,8 +9,9 @@ export default function App(props) {
             <Sidebar />
 
             <div className="main-content">
-                <div className="breakPointM-display-none" style={{ height: '80px' }}></div>
-                <div className="posFixTL zi-chrome bg-white width-100 textAlign-center boxShadow-close breakPointM-display-none" style={{ height: '80px' }}>
+                <div className="breakPointM-display-none" style={{ height: '70px' }}></div>
+
+                <div className="posFixTL zi-chrome bg-white width-100 textAlign-center boxShadow-close breakPointM-display-none" style={{ height: '70px' }}>
                     <div className="display-table width-100 height-100">
                         <div className="display-tableCell verticalAlign-middle">
                             <SidebarLogo />
